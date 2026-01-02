33469b8e1eacaee7dcdbe8aac46e4830b5b6c2ce
Robensonl <benbenlouissaint51@gmail.com>
2026-01-02 13:11:41 -0300

    port ...

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 30001,
+    port: 5173,
     open: true,
   },
   base: '/web_project_around_auth/', 
