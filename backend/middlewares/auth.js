f8e7bf7ede14305963590ea6a4479f436d15a8d3
Robensonl <benbenlouissaint51@gmail.com>
2026-01-03 23:45:47 -0300

    Update auth middleware...

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -5,7 +5,7 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res.status(401).json({ message: 'Hmmm 🤷‍♀️! lo siento se requiere autorización' });
+    return res.status(401).json({ message: 'Hmmm! lo siento se requiere autorización por favor' });
   }
 
   // Extraer token
